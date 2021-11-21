94393d52b83ddfd54432bed0f79e2d82fd50ae03
Robert Xu <u7279920@anu.edu.au>
2021-11-21 15:57:22 +1030

    Removed unused imports and comments

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -3,16 +3,11 @@ package sample;
 import javafx.application.Application;
 import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Group;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
-import javafx.scene.paint.Color;
-import javafx.scene.paint.CycleMethod;
-import javafx.scene.paint.RadialGradient;
-import javafx.scene.paint.Stop;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
 
@@ -20,30 +15,6 @@ public class Main extends Application {
 
     @Override
     public void start(Stage stage) throws Exception{
-//        String bip = "High Pressure Dave-Ny_HsSmiL9A.mp3";
-//        Media hit = new Media(new File(bip).toURI().toString());
-//        MediaPlayer mediaPlayer = new MediaPlayer(hit);
-//
-//
-//        Button button = new Button("click me");
-//        button.setOnMouseClicked(mouseEvent -> {
-//            mediaPlayer.play();
-//        });
-//
-//        Button pauseButton = new Button("pause");
-//        pauseButton.setOnMouseClicked(mouseEvent -> {
-//            mediaPlayer.pause();
-//        });
-//
-//
-//        Group root = new Group();
-//        root.getChildren().add(button);
-//        root.getChildren().add(pauseButton);
-//        Scene scene = new Scene(root, 300, 300);
-//        //Parent root = FXMLLoader.load(getClass().getResource("Main.fxml"));
-//        stage.setTitle("Hello World");
-//        stage.setScene(scene);
-//        stage.show();
 
         FXMLLoader loader = new FXMLLoader(getClass().getResource("Main.fxml"));
         Parent root = loader.load();
@@ -63,7 +34,6 @@ public class Main extends Application {
         stage.setTitle("Music App");
         stage.initStyle(StageStyle.UNDECORATED);
         stage.show();
-        //test
     }
 
 
